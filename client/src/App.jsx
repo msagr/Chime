4eaf395c05968a2a692b393c771e9a509311d237
msagr <mehulsagrawal33@gmail.com>
2024-10-14 11:04:57 +0530

    routing setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import {Button, buttonVariants} from '@/components/ui/button'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import Auth from './pages/auth';
+import Chat from './pages/chat';
+import Profile from './pages/profile';
 
 function App() {
   return (
     <div>
-      <Button> Click Me ! </Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/auth' element= {<Auth />} />
+          <Route path='/chat' element= {<Chat />} />
+          <Route path='/profile' element= {<Profile />} /> 
+          
+          <Route path='*' element= {<Navigate to="/auth" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
