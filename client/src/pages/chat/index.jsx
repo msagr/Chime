dba9b3b8e212cf29aef077ae3e5f2c72bc4cbb16
msagr <mehulsagrawal33@gmail.com>
2024-10-21 12:40:12 +0530

    chat layout

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -2,6 +2,11 @@ import { useAppStore } from "@/store";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
 import { toast } from "sonner";
+import EmptyChatContainer from "./components/empty-chat-container";
+import ContactsContainer from "./components/contacts-container";
+import ChatContainer from "./components/chat-container";
+
+
 
 const Chat = () => {
   const { userInfo } = useAppStore();
@@ -11,11 +16,13 @@ const Chat = () => {
       toast("Please setup profile to continue.");
       navigate("/profile");
     }
-  }, {userInfo, navigate});
+  }, [userInfo, navigate]);
 
   return (
-    <div>
-      Chat
+    <div className="flex h-[100vh] text-white overflow-hidden">
+      <ContactsContainer />
+      <EmptyChatContainer />
+      <ChatContainer />
     </div>
   )
 }
