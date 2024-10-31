874ddf3bdb6bf344814091fa8949853dbf2a62a5
msagr <mehulsagrawal33@gmail.com>
2024-10-31 18:09:06 +0530

    search contact functionality

---
+++ server/controllers/ContactsController.js
@@ -0,0 +1,28 @@
+import User from "../models/UserModel.js";
+
+export const searchContacts = async (request, response, next) => {
+    try {
+        const { searchTerm } = request.body;
+        
+        if(searchTerm === undefined || searchTerm === null){
+            return response.status(400).send("Search term is required.");
+        }
+
+        const sanitizedSearchTerm = searchTerm.replace(
+            /[.*+?^${}|[\]\\]/g,
+            "\\$&"
+        );
+
+        const regex = new RegExp(sanitizedSearchTerm, "i");
+        const contacts = await User.find({
+            $and: [{_id: { $ne: request.userId } }, // current user shouldn't be searched
+                {$or: [{ firstName: regex }, { lastName: regex }, {email: regex}]},
+            ], 
+        });
+
+        return response.status(200).json({ contacts });
+    } catch(error){
+        console.log({ error });
+        return response.status(500).send("Internal Server Error");
+    }
+};
--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-import authRoutes from "./routes/AuthRoutes.js"
+import authRoutes from "./routes/AuthRoutes.js";
+import contactRoutes from "./routes/ContactRoutes.js";
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/contacts", contactRoutes);
 
 const server = app.listen(port, ()=>{
     console.log(`Server is running at http://localhost:${port}`);
