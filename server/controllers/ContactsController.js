0aad354b643a0c39755a5441db100727d9ecaeb2
msagr <mehulsagrawal33@gmail.com>
2024-11-04 23:59:12 +0530

    get all contacts api

--- server/controllers/ContactsController.js
+++ server/controllers/ContactsController.js
@@ -88,3 +88,17 @@ export const getContactsforDMList = async (request, response, next) => {
         return response.status(500).send("Internal Server Error");
     }
 };
+
+export const getAllContacts = async (request, response, next) => {
+    try {
+        const users = await User.find({_id: { $ne: request.userId }}, "firstName lastName _id");
+        const contacts = users.map((user) => ({
+            label: user.firstName ? `${user.firstName} ${user.lastName}` : user.email,
+        }));
+
+        return response.status(200).json({ contacts });
+    } catch(error){
+        console.log({ error });
+        return response.status(500).send("Internal Server Error");
+    }
+};
--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getContactsforDMList, searchContacts } from "../controllers/ContactsController.js";
+import { getAllContacts, getContactsforDMList, searchContacts } from "../controllers/ContactsController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const contactRoutes = Router();
 
 contactRoutes.post("/search", verifyToken, searchContacts);
 contactRoutes.get("/get-contacts-for-dm", verifyToken, getContactsforDMList);
+contactRoutes.get("/get-all-contacts", verifyToken, getAllContacts);
 
 
 export default contactRoutes;
