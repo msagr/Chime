e9c0e5d1b41af64c653866cb4e0a51210f140f20
msagr <mehulsagrawal33@gmail.com>
2024-11-04 17:08:46 +0530

    get user contacts

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -14,3 +14,5 @@ export const CONTACTS_ROUTES = "api/contacts";
 export const SEARCH_CONTACTS_ROUTES = `${CONTACTS_ROUTES}/search`;
 export const MESSAGES_ROUTES = "api/messages";
 export const GET_ALL_MESSAGES_ROUTES = `${MESSAGES_ROUTES}/get-messages`;
+
+export const GET_DM_CONTACTS_ROUTES = `${CONTACTS_ROUTES}/get-contacts-for-dm`;
--- server/routes/ContactRoutes.js
+++ server/routes/ContactRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { searchContacts } from "../controllers/ContactsController.js";
+import { getContactsforDMList, searchContacts } from "../controllers/ContactsController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
 
 const contactRoutes = Router();
 
 contactRoutes.post("/search", verifyToken, searchContacts);
+contactRoutes.get("/get-contacts-for-dm", verifyToken, getContactsforDMList);
+
 
 export default contactRoutes;
