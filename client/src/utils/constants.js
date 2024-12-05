c02a32e972353cdc8eb9fd4bdf55076686d29f84
msagr <mehulsagrawal33@gmail.com>
2024-12-05 19:22:03 +0530

    fix deployment issue

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,6 +1,6 @@
 // All the utility functions.
 
-export const HOST = import.meta.env.VITE_SERVER_URL;
+export const HOST = "http://localhost:3000";
 export const AUTH_ROUTES = "http://localhost:3000/api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
 export const LOGIN_ROUTE = `${AUTH_ROUTES}/login`;
