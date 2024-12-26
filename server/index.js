5f4d20a2c6948d2d41436d8f0b0651b9b96aa708
msagr <mehulsagrawal33@gmail.com>
2024-12-26 10:54:18 +0530

    fixed cors issue

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const databaseURL = process.env.DATABASE_URL;
 
 app.use(
     cors({
-        origin: [process.env.ORIGIN],
+        origin: [process.env.ORIGIN, 'https://realtime-chat-65rx.onrender.com'],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
     })
